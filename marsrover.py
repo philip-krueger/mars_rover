a64346b2f5149d9702b21905780ae3abd90c251d
Philip Krueger <philip_krueger1992@web.de>
2018-04-12 13:32:43 +0200

    new functions

--- marsrover.py
+++ marsrover.py
@@ -11,6 +11,14 @@ class Rover:
         self.y = y
         self.direction = direction
 
+    def checkStartingPoint(self, start_x, start_y, start_direction):
+
+        possible_directions = ['N', 'S', 'W', 'E']
+        if start_x >= 0 <= 10 and start_y >= 0 <= 10 and start_direction in possible_directions:
+            return True
+        else:
+            return False
+
     def moving_screen(self):
         print("Mars Rover starting...")
         self.print_current_location()
@@ -22,86 +30,177 @@ class Rover:
         print("...")
         time.sleep(2)
 
-    def move_rover(self, move_command):
-
-        self.moving_screen()
-        if self.direction == 'N'or self.direction == 'S' or self.direction == 'W' or self.direction == 'E':
-
-            self.moveroverfunction(move_command)
-        else:
-            RuntimeError("Invalid Direction")
-            exit()
+    def print_obstacle_detected(self):
+        print("obstacle detected")
+        print("staying at last possible location")
+        self.print_current_location()
+        exit()
 
     def print_current_location(self):
         print("Facing Direction: "+self.direction)
         print("Location: %d,%d" % (self.x, self.y))
 
-    def moveroverfunction(self, move_command):
+    def detect_obstacle(self, moving):
+        obstacle2 = ([3, 3], [2, 2], [5, 5], [4, 5], [6, 8])
+
+        if self.direction == 'N':
+            if moving == 'f':
+                tempy = self.y
+                tempy =+ 1
+                if [self.x, tempy] in obstacle2:
+                    return True
+                else:
+                    return False
+            else:
+                tempy = self.y
+                tempy =- 1
+                if [self.x, tempy] in obstacle2:
+                    return True
+                else:
+                    return False
+        elif self.direction == 'S':
+            if moving == 'f':
+                tempy = self.y
+                tempy =- 1
+                if [self.x, tempy] in obstacle2:
+                    return True
+                else:
+                    return False
+            else:
+                tempy = self.y
+                tempy =+ 1
+                if [self.x, tempy] in obstacle2:
+                    return True
+                else:
+                    return False
+        elif self.direction == 'W':
+            if moving == 'f':
+                tempx = self.x
+                tempx =- 1
+                if [tempx, self.y] in obstacle2:
+                    return True
+                else:
+                    return False
+            else:
+                tempx = self.x
+                tempx =+ 1
+                if [tempx, self.y] in obstacle2:
+                    return True
+                else:
+                    return False
+        elif self.direction == 'E':
+            if moving == 'f':
+                tempx = self.x
+                tempx =+ 1
+                if [tempx, self.y] in obstacle2:
+                    return True
+                else:
+                    return False
+            else:
+                tempx = self.x
+                tempx =- 1
+                if [tempx, self.y] in obstacle2:
+                    return True
+                else:
+                    return False
+
+    def move_rover(self, move_command):
+        self.print_current_location()
         for moving in move_command:
 
-            #   Moving to North
+            #   North
             if self.direction == 'N':
                 #   This is for wrapping the grid around a sphere
                 #   When the Rover reaches the end of the grid it will continue on the other side, grid will be 10x10
                 #   Will be the first check on every direction
-                if moving == 'f' and self.x == 10:
+                if moving == 'f' and self.x > 10:
                     self.x = 0
-
+                #   Before every movement wether its back or forward, it will check for obstacles
+                #   in function detect_obstacles is a predefined array of obstacles
                 if moving == 'f':
-                    self.y += 1
+                    if self.detect_obstacle(moving):
+                        self.print_obstacle_detected
+                    else:
+                        self.y += 1
                 if moving == 'b':
+                    if self.detect_obstacle(moving):
+                        self.print_obstacle_detected()
+                    else:
                         self.y -= 1
                 if moving == 'r':
                     self.direction = 'E'
                 if moving == 'l':
                     self.direction = 'W'
-
+            #   South
             elif self.direction == 'S':
-                if moving == 'f' and self.y == 0:
+                if moving == 'f' and self.y < 0:
                     self.y = 10
+
                 if moving == 'f':
+                    if self.detect_obstacle(moving):
+                        self.print_obstacle_detected()
+                    else:
                         self.y -= 1
                 if moving == 'b':
-                    self.y += 1
+                    if self.detect_obstacle(moving):
+                        self.print_obstacle_detected()
+                    else:
+                        self.y += 1
                 if moving == 'r':
                     self.direction = 'W'
                 if moving == 'l':
                     self.direction = 'E'
-
+            #   East
             elif self.direction == 'E':
-                if moving == 'f' and self.x == 10:
+                if moving == 'f' and self.x > 10:
                     self.x = 0
+
                 if moving == 'f':
-                    self.x += 1
+                    if self.detect_obstacle(moving):
+                        self.print_obstacle_detected()
+                    else:
+                        self.x += 1
                 if moving == 'b':
+                    if self.detect_obstacle(moving):
+                        self.print_obstacle_detected()
+                    else:
                         self.x -= 1
                 if moving == 'r':
                     self.direction = 'S'
                 if moving == 'l':
                     self.direction = 'N'
-
+            #   West
             elif self.direction == 'W':
-                if moving == 'f' and self.x == 0:
+                if moving == 'f' and self.x < 0:
                     self.x = 10
+
                 if moving == 'f':
+                    if self.detect_obstacle():
+                        self.print_obstacle_detected()
+                    else:
                         self.x -= 1
                 if moving == 'b':
-                    self.x += 1
+                    if self.detect_obstacle():
+                        self.print_obstacle_detected()
+                    else:
+                        self.x += 1
                 if moving == 'r':
                     self.direction = 'N'
                 if moving == 'l':
                     self.direction = 'S'
 
-        if self.location_is_good():
-            print("destination reached")
-            self.print_current_location()
-        else:
-            exit()
-
-    def location_is_good(self):
-
-
-    def location_is_bad(self):
-        print("Reached obstacle")
-        print("Stopping at last possible point")
+        # One problem is when the rover reaches the end of the grid and goes over the grid
+        # If its the last value in the array it will not go in the check of the loop again
+        # So I implemented a check after the for-loop to set the values if out of grid
+        if self.direction == 'N' and self.y > 10:
+            self.y = 0
+        elif self.direction == 'S' and self.y < 0:
+            self.y = 10
+        elif self.direction == 'W'and self.x < 0:
+            self.x = 10
+        elif self.direction == 'E' and self.x > 10:
+            self.x = 0
         self.print_current_location()
+
+
+
--- test_marsrover.py
+++ test_marsrover.py
@@ -1,12 +1,24 @@
 from marsrover import Rover
 import time
 
+
 class testrover():
 
-        erover = Rover(0, 0, 'N')
         move_command1 = ['f', 'r', 'f', 'l', 'f', 'r', 'f']  # output should be 2,2 E
         move_command2 = ['f', 'r', 'f', 'l', 'f', 'l', 'f', 'f']  # try to go over the grid in negativ
-        Rover.move_rover(erover, move_command2)
+        move_command3 = ['f', 'f', 'f', 'r', 'f', 'f' 'f']  # Should reach an obstacle
 
-        obstacle1 = [2, 2]
-        obstacle2 = ([3, 3], [2, 2], [5, 5], [4, 5], [6, 8],)
+        # Function checkStartinPoint takes initial pararmeters and checks them for their validity
+        start_x = 0
+        start_y = 0
+        start_direction = 'N'
+
+        if Rover.checkStartingPoint(Rover, start_x, start_y, start_direction):
+
+            erover = Rover(start_x, start_y, start_direction)
+            Rover.move_rover(erover, move_command1)
+
+        else:
+            print("Wrong Starting Points and/or direction")
+            exit()
+
